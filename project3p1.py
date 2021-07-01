6b68a3e66697a0f1edaf87df9ffc208bb65acc49
Sheena <sheena.takkenberg@gmail.com>
2021-07-01 22:20:40 +1000

    project 3.1 - can't believe I got this to work!

---
+++ project3p1.py
@@ -0,0 +1,14 @@
+hrs = input("Enter Hours:")
+h = float(hrs)
+rate = input("Enter Rate:")
+r = float(rate)
+overtimeRate = 10.5*1.5
+pay = 40 * r
+
+if h > 40:
+    overtime = h - 40
+    oPay = overtimeRate * overtime + pay
+    print ("Pay: ", oPay)
+    print("Overtime hours worked: ", overtime)
+else:
+    print("Pay: ", pay)
